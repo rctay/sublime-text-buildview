61f181acd0ced56f8f8c403e6da52060cdcf328b
Tay Ray Chuan <rctay89@gmail.com>
2013-03-09 01:49:14 +0800

    describe technique source
    instead of just plonking a url/sha.

--- commands.py
+++ commands.py
@@ -11,7 +11,11 @@ class BuildListener(sublime_plugin.EventListener, PipeViews):
         if self.dest_view and self.dest_view.id() == view.id():
             self.dest_view = None
 
-    # source: https://github.com/kemayo/sublime-text-2-clipboard-history/blob/ed5cac2a50189f2399e928b4142b19506af5cc6f#clipboard.py
+    # The technique used below of hooking on to an existing (possibly built-
+    # in) command was based on kemayo's excellent work [1]. The comment
+    # describing the technique is reproduced here.
+    #
+    # [1] https://github.com/kemayo/sublime-text-2-clipboard-history/blob/ed5cac2a50189f2399e928b4142b19506af5cc6f#clipboard.py
     #
     # Here we see a cunning plan. We listen for a key, but never say we
     # support it. This lets us respond to ctrl-c and ctrl-x, without having
